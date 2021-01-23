27e47b7dd80811ec95e54f1b9290191b3d4937c2
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-01-24 03:56:30 +0530

    some minor changes

--- src/gameData.tsx
+++ src/gameData.tsx
@@ -10,24 +10,25 @@ export interface IBlockID {
     'add-i-to-sum': string;
     'display-sum': string;
     'divide-sum-by-n': string;
-    'input-x':string;
-    'multiply-x':string;
-    'print-x':string;
-    '}':string;
-    '{':string;
-    'print-s':string;
-    's[i]-lower':string;
-    'else':string;
-    's[i]-upper':string;
-    'if-x.lower':string;
-    'input-s':string;
-    'printno':string;
-    'printyes':string;
-    'input-i':string;
-    'if-n%4':string;
-    'if-j-a[i]':string;
-    'print-j':string;
-    'init-j':string;
+    'input-x': string;
+    'multiply-x': string;
+    'print-x': string;
+    '}': string;
+    '{': string;
+    'print-s': string;
+    's[i]-lower': string;
+    else: string;
+    's[i]-upper': string;
+    'if-x.lower': string;
+    'input-s': string;
+    printno: string;
+    printyes: string;
+    'input-i': string;
+    'if-n%4': string;
+    'if-j-a[i]': string;
+    'print-j': string;
+    'init-j': string;
+    'input-n': string;
 }
 
 export interface ICodeBlock {
@@ -53,11 +54,26 @@ export const problems: IProblem[] = [
         level: 1,
         problemStatement: 'Find the square of a number.',
         initBlocks: [
-            { blockType: 'start', code: 'START',blockID: 'start',uid:'0x0' },
-            { blockType: 'io', code: 'x <- input', blockID: 'input-x',uid:'0x1' },
-            { blockType: 'exec', code: 'y <- x * x', blockID: 'multiply-x',uid:'0x2' },
-            { blockType: 'io', code: 'print y', blockID: 'print-x',uid:'0x3' },
-            { blockType: 'end', code: 'END', blockID: 'end',uid:'0x4'},
+            { blockType: 'start', code: 'START', blockID: 'start', uid: '0x0' },
+            {
+                blockType: 'io',
+                code: 'x <- input',
+                blockID: 'input-x',
+                uid: '0x1',
+            },
+            {
+                blockType: 'exec',
+                code: 'y <- x * x',
+                blockID: 'multiply-x',
+                uid: '0x2',
+            },
+            {
+                blockType: 'io',
+                code: 'print y',
+                blockID: 'print-x',
+                uid: '0x3',
+            },
+            { blockType: 'end', code: 'END', blockID: 'end', uid: '0x4' },
         ],
         validation: ['start', 'input-x', 'multiply-x', 'print-x', 'end'],
         placeHolderDetails: [
@@ -72,19 +88,39 @@ export const problems: IProblem[] = [
         level: 2,
         problemStatement: 'Check if number is a multiple of 4.',
         initBlocks: [
-            { blockType: 'start', code: 'START', blockID: 'start',uid:'893' },
-            { blockType: 'cond', code: 'if n % 4==0', blockID: 'if-n%4',uid:'894' },
-            { blockType: 'cond', code: '{', blockID: '{',uid:'895' },
-            { blockType: 'io', code: 'n<-input', blockID: 'input-i',uid:'896' },
-            { blockType: 'cond', code: '}', blockID: '}',uid:'897'},
-            { blockType: 'io', code: 'print YES', blockID: 'printyes',uid:'897'},
-            { blockType: 'cond', code: 'else', blockID: 'else',uid:'898'},
-            { blockType: 'io', code: 'print NO', blockID: 'printno',uid:'899'},
+            { blockType: 'start', code: 'START', blockID: 'start', uid: '893' },
+            {
+                blockType: 'cond',
+                code: 'if n % 4 == 0',
+                blockID: 'if-n%4',
+                uid: '894',
+            },
+            { blockType: 'cond', code: '{', blockID: '{', uid: '895' },
+            {
+                blockType: 'io',
+                code: 'n <- input',
+                blockID: 'input-n',
+                uid: '896',
+            },
+            { blockType: 'cond', code: '}', blockID: '}', uid: '897' },
+            {
+                blockType: 'io',
+                code: 'print YES',
+                blockID: 'printyes',
+                uid: '897',
+            },
+            { blockType: 'cond', code: 'else', blockID: 'else', uid: '898' },
+            {
+                blockType: 'io',
+                code: 'print NO',
+                blockID: 'printno',
+                uid: '899',
+            },
             {
                 blockType: 'end',
                 code: 'END',
                 blockID: 'end',
-                uid:'900'
+                uid: '900',
             },
         ],
         validation: [
@@ -115,41 +151,51 @@ export const problems: IProblem[] = [
         problemStatement:
             'Swap lowecase characters to uppercase and uppercase to lowercase.',
         initBlocks: [
-            { blockType: 'start', code: 'START', blockID: 'start',uid:'kom' },
-            { blockType: 'io', code: 'String<-input', blockID: 'input-s',uid:'9u8593' },
+            { blockType: 'start', code: 'START', blockID: 'start', uid: 'kom' },
+            {
+                blockType: 'io',
+                code: 'String<-input',
+                blockID: 'input-s',
+                uid: '9u8593',
+            },
             {
                 blockType: 'loop',
                 code: 'Loop i=0->len(String)',
                 blockID: 'loop-i-0-n',
-                uid:'8493'
-
+                uid: '8493',
             },
-            { blockType: 'cond', code: '{', blockID: '{' ,uid:'98792'},
+            { blockType: 'cond', code: '{', blockID: '{', uid: '98792' },
             {
                 blockType: 'cond',
                 code: 'if String[i] is Lowercase',
                 blockID: 'if-x.lower',
-                uid:'kpop'
+                uid: 'kpop',
             },
-            { blockType: 'cond', code: '{', blockID: '{',uid:'09-0' },
+            { blockType: 'cond', code: '{', blockID: '{', uid: '09-0' },
             {
                 blockType: 'exec',
                 code: 'String[i]=String[i] to uppercase',
-                blockID: 's[i]-upper',uid:'58604'
+                blockID: 's[i]-upper',
+                uid: '58604',
             },
-            { blockType: 'cond', code: '}', blockID: '}',uid:'644'},
-            { blockType: 'cond', code: 'Else', blockID: 'else',uid:'utr' },
-            { blockType: 'cond', code: '{', blockID: '{',uid:'9876' },
+            { blockType: 'cond', code: '}', blockID: '}', uid: '644' },
+            { blockType: 'cond', code: 'Else', blockID: 'else', uid: 'utr' },
+            { blockType: 'cond', code: '{', blockID: '{', uid: '9876' },
             {
                 blockType: 'exec',
                 code: 'String[i]=String[i] to lowercase',
                 blockID: 's[i]-lower',
-                uid:'rwsir'
+                uid: 'rwsir',
             },
-            { blockType: 'cond', code: '}', blockID: '}',uid:'12i' },
-            { blockType: 'cond', code: '}', blockID: '}',uid:'1246' },
-            { blockType: 'io', code: 'print String', blockID: 'print-s',uid:'455q' },
-            { blockType: 'end', code: 'END', blockID: 'end',uid:'4256' },
+            { blockType: 'cond', code: '}', blockID: '}', uid: '12i' },
+            { blockType: 'cond', code: '}', blockID: '}', uid: '1246' },
+            {
+                blockType: 'io',
+                code: 'print String',
+                blockID: 'print-s',
+                uid: '455q',
+            },
+            { blockType: 'end', code: 'END', blockID: 'end', uid: '4256' },
         ],
         validation: [
             'start',
@@ -297,10 +343,25 @@ export const problems: IProblem[] = [
                 blockID: 'end',
                 uid: '000',
             },
-            { blockType: 'cond', code: 'if j==array[i]', blockID: 'if-j-a[i]',uid:'78593' },
-            { blockType: 'io', code: 'print j', blockID: 'print-j',uid:'980i'},
-            { blockType: 'exec', code: 'int j=array[0]', blockID: 'init-j',uid:'123r4' },
-            { blockType: 'end', code: 'END', blockID: 'end',uid:'789' },
+            {
+                blockType: 'cond',
+                code: 'if j==array[i]',
+                blockID: 'if-j-a[i]',
+                uid: '78593',
+            },
+            {
+                blockType: 'io',
+                code: 'print j',
+                blockID: 'print-j',
+                uid: '980i',
+            },
+            {
+                blockType: 'exec',
+                code: 'int j=array[0]',
+                blockID: 'init-j',
+                uid: '123r4',
+            },
+            { blockType: 'end', code: 'END', blockID: 'end', uid: '789' },
         ],
         validation: [
             'start',
@@ -345,7 +406,6 @@ export const problems: IProblem[] = [
         validation: [],
     },
 ];
-
 
 interface IFooterIcon {
     blockType: keyof Block;
